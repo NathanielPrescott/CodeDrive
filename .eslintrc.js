1d7a0b0cc8d1f6d8f44ea52f2b68e6c7fbeb9669
Nathan Prescott <echodog@gmail.com>
2019-11-16 12:11:14 -0800

    minor cleanup after removing pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     node: true,
-    jest: true,
+    jest: true
   },
   extends: ['eslint:recommended',
             'plugin:vue/recommended'],
--- service/rectangle.js
+++ service/rectangle.js
@@ -1,7 +1,7 @@
 export class Rectangle {
   constructor(height, width) {
     this.height = height;
-    this.width = width
+    this.width = width;
   }
 
   get area() {
